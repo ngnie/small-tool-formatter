85e4d2b43501dc18411a320441b666b91efd6c30
Nikolaj G. Nielsen <nikolaj.groenlund@yahoo.com>
2025-04-15 11:40:45 +0200

    added unit test for left result adder and left token adder.

--- adapters/formatter-adapter/src/main/java/com/pub/adapter/formatter/left/LeftResultAdder.java
+++ adapters/formatter-adapter/src/main/java/com/pub/adapter/formatter/left/LeftResultAdder.java
@@ -1,11 +1,15 @@
 package com.pub.adapter.formatter.left;
 
 import com.pub.adapter.formatter.common.ResultAdder;
+import java.util.Objects;
 
 public class LeftResultAdder implements ResultAdder {
 
     @Override
     public void add(StringBuilder alignment, StringBuilder result) {
+        Objects.requireNonNull(alignment, "Trying to add alignment to result. Alignment is null");
+        Objects.requireNonNull(result, "Trying to add alignment to result. Result is null");
+
         if (!alignment.isEmpty()) {
             result.append(alignment).append("\n");
         }
--- adapters/formatter-adapter/src/main/java/com/pub/adapter/formatter/left/LeftTokenAdder.java
+++ adapters/formatter-adapter/src/main/java/com/pub/adapter/formatter/left/LeftTokenAdder.java
@@ -1,11 +1,14 @@
 package com.pub.adapter.formatter.left;
 
 import com.pub.adapter.formatter.common.TokenAdder;
+import java.util.Objects;
 
 public class LeftTokenAdder implements TokenAdder {
 
     @Override
     public void add(StringBuilder sb, String token) {
+        Objects.requireNonNull(sb, "Trying to add token to result. sb is null.");
+        Objects.requireNonNull(token, "Trying to add token to result. token is null.");
         sb.append(token);
     }
 }
